1ccf397fe64345c65121237ad50d121bbfc31fbb
MichielAdriaansens <m.adriaansens@gmail.com>
2022-07-28 00:34:23 +0200

    Ae momma Testing2 basic ERC-20

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,5 +4,14 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract Token{
-    string public constant name = "NiggaToken";
+    string public name;
+    string public symbol;
+    uint256 public decimals = (10**18);
+    uint256 public totalSupply;
+
+    constructor(string memory _name, string memory _sym,uint256 _amount){
+        name = _name;
+        symbol = _sym;
+        totalSupply = _amount * decimals;
+    }
 }
--- test/Token.js
+++ test/Token.js
@@ -1,21 +1,45 @@
 const { assert } = require("chai")
+//const { expect } = require("chai")
 const { ethers } = require("hardhat")
 
+const tokens = (_amount) => {
+   return ethers.utils.parseUnits(`${_amount}`, "ether").toString()
+}
+
 describe("Token", ()=>{
-    //Test Yo Ssshit boii.
-    it("Contract has name",async ()=>{
+    let token;
+
+    beforeEach(async ()=>{
         //fetch the Token Contract
         const Token = await ethers.getContractFactory("Token") //dit is een referentie naar het Contract.
-        const token = await Token.deploy() //om goed te testen moet je het contract eerst deployen. 
-
-        //reach in and get that contract name
-        const name = await token.name()
-        
-        //code that checks if name of Contract matches
-        assert.equal(name,"NiggaToken")
-        //
-        if(name == "NiggaToken"){
-            console.log(name)
-        }
+        token = await Token.deploy("NiggaToken","NT",1000000) //om goed te testen moet je het contract eerst deployen. 
     })
+
+    describe("Deployment",() => {
+        const name = "NiggaToken"
+        const symbol = "NT"
+        const decimal = 10**18
+        const tSupply = tokens(1000000)
+
+        //Test Yo Ssshit boii.
+        it("has name",async ()=>{
+            assert.equal(await token.name(),name)
+            console.log(`contract name: ${name}`)
+
+        })
+        it("has symbol", async () => {
+            assert.equal(await token.symbol(), symbol)
+            console.log(`contract symbol: ${await token.symbol()}`);
+        })
+        it("has decimals", async () => {
+            assert.equal(await token.decimals(), decimal)
+            console.log(await token.decimals());
+        })
+        it("has total supply", async () => { 
+        // expect(await token.totalSupply()).to.equal(value)
+            assert.equal(await token.totalSupply(), tSupply) //..assert*werkt wel, value omzetten naar string:p
+            console.log(await token.totalSupply());
+        })
+    })
+
 })
