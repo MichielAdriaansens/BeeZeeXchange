7c936d2c62ab5b888f5f8e1ce20fda3dc4c6cc40
MichielAdriaansens <m.adriaansens@gmail.com>
2022-09-18 15:51:54 +0200

    4_UI Navbar react-redux

--- src/components/App.js
+++ src/components/App.js
@@ -12,6 +12,7 @@ import {
   loadTokens,
   loadExchange
 } from '../store/interactions';
+import Navbar from './Navbar';
 
 
 function App() {
@@ -20,19 +21,27 @@ function App() {
 
   async function loadBlockChainData() {
     //provider
-    const provider =await loadProvider(dispatch);
+    const provider = await loadProvider(dispatch);
     
     //get network 31337
     const chainId = await loadNetwork(dispatch, provider);
      console.log("network Id: ", chainId);
 
-    //load account and get balance form metamask
-    const account =await loadAccount(provider, dispatch);
-    console.log("account from MetaMask: ",account);
+    //reload on network change
+    window.ethereum.on('networkChanged', async () => {
+      window.location.reload();
+    })
+
+    //load account and get balance form metamask when changed
+    window.ethereum.on('accountsChanged', async () => {
+      await loadAccount(provider, dispatch);
+    })
+    //const account =await loadAccount(provider, dispatch);
+    //console.log("account from MetaMask: ",account);
 
     //Token Contract
     const token = await loadTokens([config[chainId].BCC.address, config[chainId].HIP.address], provider, dispatch);
-    console.log("Token Adress: ", config[chainId].BCC.address);
+  //  console.log("Token Adress: ", config[chainId].BCC.address);
     console.log("Token name: ", await token.name());
     console.log("Token symbol: ", await token.symbol())
     console.log('total amount: ', ethers.utils.formatEther(await token.totalSupply()));
@@ -50,7 +59,7 @@ function App() {
     <div>
 
       {/* Navbar */}
-
+      <Navbar/>
       <main className='exchange grid'>
         <section className='exchange__section--left grid'>
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,81 @@
+import { useSelector, useDispatch } from "react-redux";
+import logo from "../assets/logo.png";
+import ethLogo from "../assets/eth.svg"
+import Blockies from 'react-blockies';
+import {loadAccount} from '../store/interactions'
+
+import config from '../config.json'
+
+function Navbar(){
+    const provider = useSelector(state => state.provider.connection);
+    const chainId = useSelector(state => state.provider.chainId);
+    const account = useSelector((state)=>{return state.provider.account});
+    const balance = useSelector(state => state.provider.balance);
+
+    const dispatch = useDispatch();
+
+    async function connectHandler(){
+        await loadAccount(provider, dispatch);
+    }
+    async function networkHandler(event){
+        console.log("switch up! ", event.target.value);
+        await window.ethereum.request({
+            method: 'wallet_switchEthereumChain',
+            params: [{chainId: event.target.value}] 
+        })
+    }
+
+    return(
+       <div className='exchange__header grid'>
+        <div className='exchange__header--brand flex'>
+           <img src = {logo} className = 'logo' alt="beezee logo"/>
+           <h1>BeeZee exchange</h1>
+        </div>
+  
+        <div className='exchange__header--networks flex'>
+            <img src={ethLogo} alt="ethlogo"/>
+
+            {chainId && (
+                <select name="networks" id="networks" value={config[chainId]? `0x${chainId.toString(16)}` : "0"} onChange={networkHandler}>
+                <option value={0} disabled>select network</option>
+                <option value="0x7A69">local host</option>
+                <option value="0x2a">Kovan</option>
+            </select>
+            )}
+
+        </div>
+  
+        <div className='exchange__header--account flex'>
+            {
+                balance? (<p><small>balance: </small>{Number(balance).toFixed(4)} ETH</p>)
+                :
+                (<p><small>balance: </small>0 ETH</p>) 
+            }
+            {
+                //..? ... : ...  turnary is hetzelfde als een if statement. werkt met react-html code
+                account?  (
+                    <a
+                    href={config[chainId]? `${config[chainId].explorerUrl}/address/${account}`: "#"}
+                    target = "_blank"
+                    rel= "noreferrer"
+                    > 
+                    {account.slice(0,5) + "..." + account.slice(38,42)}
+                <Blockies 
+                    seed={account}
+                    size = {10}
+                    scale={3}
+                    color='#2187D0'
+                    bgColor='#F1F2F9'
+                    spotColor='#767F92'
+                    className='identicon'
+                />
+                </a>) 
+                : 
+                (<button className="button" onClick={connectHandler}>Connect</button>)
+            }
+        </div>
+      </div>
+    )
+}
+
+export default Navbar;
